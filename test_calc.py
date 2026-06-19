d22736d6b00fd310270a1c2d16c790c9108b2fa2
Akmuhammetpro <jorayew.pro@gmail.com>
2026-06-19 22:21:34 +0300

    Special Error divide fix

--- test_calc.py
+++ test_calc.py
@@ -4,8 +4,8 @@ from calc import add, divide
 def test_add():
     assert add(2, 3) == 5
 
-def divide(a, b):
-    return a / b
+def test_divide():
+    assert divide(10, 2) == 5.0
 
 def test_divide_by_zero():
     with pytest.raises(ValueError):
